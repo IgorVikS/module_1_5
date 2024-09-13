945f1d728dea068cac3687605aac5c4bfa5f7a26
Игорь <temruk_siv@mail.ru>
2024-09-13 21:59:21 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,13 @@
+immutable_var = 1, 2, 3, 'a', 'b', 'c'
+print(immutable_var)
+
+# immutable_var = 1, 2, 3, 'a', 'b', 'c'
+# immutable_var[0] = 5   кортеж не поддерживает обращение по элементам
+# print(immutable_var)  TypeError: 'tuple' object does not support item assignment
+
+mutable_list = [1, 2, 3, 'a', 'b', 'c', 'Modifieds']
+print(mutable_list)
+
+mutable_list = [1, 2, 3, 'a', 'b', 'c'], 'Modifieds'
+mutable_list[0][4] = 7
+print(mutable_list)
